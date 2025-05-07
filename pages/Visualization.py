1c5ecbd98578e1fa8dd4829d72d92a2dc0adb46d
Hadi Shaheen <hadishaheen736@gmail.com>
2025-05-07 07:12:58 +0300

    Update Visualization.py

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -57,7 +57,7 @@ fig_store_types = px.pie(
     names='Type',
     values='Count',
     title='Weekly Sales by Store Type',
-    color_discrete_sequence=px.colors.qualitative.Bold
+    color_discrete_sequence=px.colors.qualitative.Set1
 )
 
 # Plot 2: Sales by Year
